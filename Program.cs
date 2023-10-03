9c8738bc824e8847d82d6731c3de10f5667fc9b2
Alex <alex.gmaia98@gmail.com>
2023-10-02 22:47:01 -0300

    Correção classes e metodos desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: '83442233', modelo: "tijolo", imei: '123445566', memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: '83442233', modelo: "liso", imei: '123777776', memoria: 62);
+iphone.Ligar();
+iphone.InstalarAplicativo("tetris");
